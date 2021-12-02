6ed35bf09a1a6c9061267d5205a146ffd3a66a97
Richard Goodman <richardg@brandwatch.com>
2021-12-02 10:10:28 +0000

    Complete day 2 part 2 - Python

--- python/day-two/main.py
+++ python/day-two/main.py
@@ -12,21 +12,27 @@ def main():
     directory = os.path.dirname(os.path.relpath(__file__))
     input_file = join(directory, INPUT_FILE_NAME)
 
+    # For simplicity I'm gonna keep depth as is, and swap the usage than the
+    # task orders
     horizontal = 0
     depth = 0
+    aim = 0
     data = _get_data(input_file)
 
     for instruction in data:
         if instruction[0] == "forward":
             horizontal += int(instruction[1])
+            aim += (depth * int(instruction[1]))
         if instruction[0] == "up":
             depth -= int(instruction[1])
         if instruction[0] == "down":
             depth += int(instruction[1])
-    print("End 'co-ords': Horizontal: {horizontal}, Depth: {depth}\n\t{product}".format(
+    print("End 'co-ords': Horizontal: {horizontal}, Depth: {depth}, Aim: {aim}\n\t{product_one}\n\t{product_two}".format(
         horizontal=horizontal,
         depth=depth,
-        product=(horizontal * depth)
+        aim=aim,
+        product_one=(horizontal * depth),
+        product_two=(horizontal * aim)
     ))
 
 
